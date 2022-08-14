3097a8d03adfb7a39914ef0d32a8919f521b0ebe
farisamirmudin <faris071298@gmail.com>
2022-08-15 01:50:14 +0200

    added install file

--- linux/install/install.sh
+++
@@ -1,6 +0,0 @@
-curl -0 https://github.com/PowerShell/PowerShell/releases/download/v7.2.4/powershell-lts_7.2.4-1.deb_amd64.deb
-sudo dpkg -i powershell-lts_7.2.4-1.deb_amd64.deb
-sudo apt install -f
-sudo apt install git-all
-sudo apt install fzf
-sudo apt install mpv
